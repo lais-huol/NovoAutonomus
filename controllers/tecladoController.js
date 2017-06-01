6ae41d99d65fb0f4a32f4f45096501f39950465d
Pablo Holanda <phol4nda@gmail.com>
2017-06-01 14:06:25 -0300

    adicionado espaco

--- controllers/tecladoController.js
+++ controllers/tecladoController.js
@@ -11,7 +11,7 @@ app.controller("tecladoController", function($scope, $location, dbService){
     $scope.alfabeto = [['A', 'B', 'C', 'D', 'E', 'F', 'G'],
                       ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
                       ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
-                      ['V', 'W', 'X', 'Y', 'Z', 'Ç',],
+                      ['V', 'W', 'X', 'Y', 'Z', 'Ç', '--'],
                       ['1', '2', '3', '4', '5', '6', '7'],
                       ['8', '9', '0', '. ', ', ', '? ', '! ']];
 
@@ -55,7 +55,7 @@ app.controller("tecladoController", function($scope, $location, dbService){
           $scope.alfabeto = [['A', 'B', 'C', 'D', 'E', 'F', 'G'],
                             ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
                             ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
-                            ['V', 'W', 'X', 'Y', 'Z', 'Ç',],
+                            ['V', 'W', 'X', 'Y', 'Z', 'Ç', '--'],
                             ['1', '2', '3', '4', '5', '6', '7'],
                             ['8', '9', '0', '. ', ', ', '? ', '! ']];
         }
@@ -63,7 +63,7 @@ app.controller("tecladoController", function($scope, $location, dbService){
           $scope.alfabeto = [['a', 'b', 'c', 'd', 'e', 'f', 'g'],
                             ['h', 'i', 'j', 'k', 'l', 'm', 'n'],
                             ['o', 'p', 'q', 'r', 's', 't', 'u'],
-                            ['v', 'w', 'x', 'y', 'z', 'ç',],
+                            ['v', 'w', 'x', 'y', 'z', 'ç', '--'],
                             ['1', '2', '3', '4', '5', '6', '7'],
                             ['8', '9', '0', '. ', ', ', '? ', '! ']];
         }
